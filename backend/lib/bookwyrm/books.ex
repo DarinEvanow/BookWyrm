8542ef4d859ea4ad1bc61dfee458500558548792
Darin Evanow <darin.evanow@gusto.com>
2020-01-11 16:44:11 -0800

    Add context function and schema query to get a list of all books in either ascending or descending order.

--- backend/lib/bookwyrm/books.ex
+++ backend/lib/bookwyrm/books.ex
@@ -46,10 +46,22 @@ defmodule Bookwyrm.Books do
   end
 
   @doc """
-  Returns a list of all authors.
+  Returns a list of all books matching the given `criteria.
+
+  Example Criteria:
+  [{:limit, 15}, {:order, :asc}]
   """
-  def list_books do
-    Repo.all(Book)
+  def list_books(criteria) do
+    query = from(b in Book)
+
+    Enum.reduce(criteria, query, fn
+      {:limit, limit}, query ->
+        from(b in query, limit: ^limit)
+
+      {:order, order}, query ->
+        from(b in query, order_by: [{^order, :id}])
+    end)
+    |> Repo.all()
   end
 
   @doc """
--- backend/lib/bookwyrm_web/schema/schema.ex
+++ backend/lib/bookwyrm_web/schema/schema.ex
@@ -11,13 +11,28 @@ defmodule BookwyrmWeb.Schema.Schema do
         {:ok, Books.get_book_by_slug!(slug)}
       end)
     end
+
+    @desc "Get a list of books"
+    field :books, list_of(:book) do
+      arg(:limit, :integer)
+      arg(:order, type: :sort_order, default_value: :asc)
+
+      resolve(fn _, args, _ ->
+        {:ok, Books.list_books(args)}
+      end)
+    end
+  end
+
+  enum :sort_order do
+    value(:asc)
+    value(:desc)
   end
 
   object :book do
-    field :id, non_null(:id)
-    field :title, non_null(:string)
-    field :description, non_null(:string)
-    field :isbn13, non_null(:integer)
-    field :slug, non_null(:string)
+    field(:id, non_null(:id))
+    field(:title, non_null(:string))
+    field(:description, non_null(:string))
+    field(:isbn13, non_null(:integer))
+    field(:slug, non_null(:string))
   end
 end
