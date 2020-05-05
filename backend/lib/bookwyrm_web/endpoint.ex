62871fe2878681b4e873a13ded2d00a06879e6e1
Darin Evanow <darin.evanow@gusto.com>
2020-05-04 19:41:39 -0700

    Create an Apollo client and wrapped the app in it. Add in a simple query, GET_BOOKS_QUERY, in Home to test the connection.

--- backend/lib/bookwyrm_web/endpoint.ex
+++ backend/lib/bookwyrm_web/endpoint.ex
@@ -1,44 +1,50 @@
 defmodule BookwyrmWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :bookwyrm
 
-  socket "/socket", BookwyrmWeb.UserSocket,
+  socket("/socket", BookwyrmWeb.UserSocket,
     websocket: true,
     longpoll: false
+  )
+
+  plug(CORSPlug)
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
   # when deploying your static files in production.
-  plug Plug.Static,
+  plug(Plug.Static,
     at: "/",
     from: :bookwyrm,
     gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    plug Phoenix.CodeReloader
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.RequestId
-  plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
+  plug(Plug.RequestId)
+  plug(Plug.Telemetry, event_prefix: [:phoenix, :endpoint])
 
-  plug Plug.Parsers,
+  plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
+  plug(Plug.Session,
     store: :cookie,
     key: "_bookwyrm_key",
     signing_salt: "er4yDkYo"
+  )
 
-  plug BookwyrmWeb.Router
+  plug(BookwyrmWeb.Router)
 end
--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const GET_BOOKS_QUERY = gql`
+  query GetBooks {
+    books {
+      title
+    }
+  }
+`;
+
+interface Data {
+  books: Array<{ title: string }>;
+}
+
+const Loading: React.FC = () => {
+  return <div>Loading</div>;
+};
+
+const Error: React.FC = () => {
+  return <div>Error</div>;
+};
+
+const Result: React.FC<{ books: any }> = ({ books }) => {
+  return books.map((book: any) => <div>{book.title}</div>);
+};
 
 const Home: React.FC = () => {
-  return <div>Hello from home!</div>;
+  return (
+    <Query<Data> query={GET_BOOKS_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <Loading />;
+        if (error) {
+          console.log(error);
+          return <Error />;
+        }
+        return <Result books={data?.books} />;
+      }}
+    </Query>
+  );
 };
 
 export default Home;
