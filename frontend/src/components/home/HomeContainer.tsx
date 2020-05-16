7c478e71a14d1f4099dde2431b73ed472d065f40
Darin Evanow <darin.evanow@gusto.com>
2020-05-15 23:41:44 -0700

    Setup React Testing Library.

--- frontend/src/components/home/HomeContainer.tsx
+++ frontend/src/components/home/HomeContainer.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Book } from '../../common/types';
+
 import Home from './Home';
+import Error from '../error/Error';
+import Loading from '../loading/Loading';
+
+export const GET_BOOKS_QUERY = gql`
+  query GetBooks {
+    books {
+      id
+      title
+    }
+  }
+`;
 
 const HomeContainer: React.FC = () => {
-  return <Home />;
+  return (
+    <Query<{ books: Book[] }> query={GET_BOOKS_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <Loading />;
+        if (error) return <Error error={error} />;
+        return <Home books={data?.books} />;
+      }}
+    </Query>
+  );
 };
 
 export default HomeContainer;
